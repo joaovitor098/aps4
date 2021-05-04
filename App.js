672515f40283196deac35a1984f351a38bedf335
João Vítor Alves <joaovitoram01@gmail.com>
2021-05-04 00:33:09 -0300

    aps finalizada

--- App.js
+++ App.js
@@ -1,112 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import Card from './src/components/cardList';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Card />
+      <View style={styles.body}>
+        <TouchableOpacity style={styles.btnFinalizar}>
+          <Text style={styles.txtBtnFinalizar}>Finalizar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnFinalizarPedido}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                'https://images-ext-2.discordapp.net/external/j8Cem0xGa1v9j5BG5YjYBsd_uk6GsKrmmh6EMXPFGq4/%3Fcaw%3D800/https/stat.ameba.jp/user_images/20181004/23/plusmarket-kanda/e9/8d/p/o0656058914278182582.png?width=522&height=469',
+            }}
+          />
+          <Text style={styles.txtBtnFinalizar}>Finalizar Pedido</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginHorizontal: '10%',
+    marginTop: '10%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  btnFinalizar: {
+    alignItems: 'center',
+    backgroundColor: '#c7c7c7',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: '5%',
+    borderWidth: 2,
+    borderColor: '#000',
   },
-  sectionDescription: {
-    marginTop: 8,
+  txtBtnFinalizar: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
   },
-  highlight: {
-    fontWeight: '700',
+  btnFinalizarPedido: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#8CD55D',
+    borderRadius: 8,
+    padding: 15,
+    marginVertical: '5%',
+    borderWidth: 2,
+    borderColor: '#000',
+    justifyContent: 'space-around',
+  },
+  image: {
+    height: 50,
+    width: 50,
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
 });
-
-export default App;
